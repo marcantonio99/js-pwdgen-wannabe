86192e337c2449229a30844922667cb82eff6604
marcantonio99 <marcantonio8799@gamil.com>
2022-10-18 16:29:54 +0200

    end

---
+++ main.js
@@ -0,0 +1,17 @@
+let name = prompt('Scrivimi il tuo nome:');
+console.log(name);
+
+let surname = prompt('Scrivimi il tuo cognome:');
+console.log(surname);
+
+let preferitColor = prompt('Scrivimi il tuo colore preferito:')
+console.log(preferitColor);
+
+let number = prompt('aggiungi un numero');
+console.log(number);
+
+let password = `${name + surname + preferitColor + number}`;
+console.log(password);
+
+let frase = `la tua password terribile : ${password}`;
+document.getElementById('passwordGen').innerHTML = `la tua password terribile : ${password}`;
